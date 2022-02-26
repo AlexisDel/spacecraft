4c783d65bfc90a12281aedb08a6db65136db7114
ThomasDelepine <thomas.delepine@live.fr>
2022-02-26 14:54:45 +0100

    coorection d'un bug sur la hitbox dans la vue des aliens

--- src/Model/GameConstants.java
+++ src/Model/GameConstants.java
@@ -4,11 +4,11 @@ public class GameConstants {
 
     /** Hauteur du terrain de jeu */
     // 40 * 2^n
-    public static final int BOARD_SIZE = 40;
+    public static final int BOARD_SIZE = 320;
     /** Dimension d'une montagne en nombre de case */
-    public static final int MOUNTAIN_SIZE = 1;
+    public static final int MOUNTAIN_SIZE = 16;
     /** Rayon autour des SpaceMarines représentant la peur des Aliens*/
-    public static final int fearOfSpaceMarines = 4;
+    public static final int fearOfSpaceMarines = 8;
 
 
 
